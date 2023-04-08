2bb0a85ad71c2ff7c066f495d385a71b9920d973
mallvinorz <mallvino1216@gmail.com>
2023-04-08 12:32:56 +0700

    add Map-ToMutableMap.kt

---
+++ src/main/kotlin/KotlinCollection/Map-ToMutableMap.kt
@@ -0,0 +1,24 @@
+package KotlinCollection
+
+fun main() {
+    val groupMap = mapOf(
+        1 to "Fender Stratocaster",
+        2 to "Fender Telecaster",
+        3 to "Squier Stratocaster",
+        4 to "Squier Telecaster",
+        5 to "Fender Jaguar",
+        6 to "Squier Jaguar",
+        7 to "Takamine",
+        8 to "Taylor",
+    )
+
+    println(groupMap)
+
+    val mutable = groupMap.toMutableMap()
+    println(mutable)
+
+    mutable[3] = "Schecter Nick Johnston"
+    mutable.put(9, "Yamaha")
+
+    println(mutable)
+}
