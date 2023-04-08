3adce042d279217130f23a12affbc62624c2ce27
mallvinorz <mallvino1216@gmail.com>
2023-04-08 12:08:28 +0700

    add Function.kt

---
+++ src/main/kotlin/KotlinFundamental/Function.kt
@@ -0,0 +1,11 @@
+package KotlinFundamental
+
+fun main() {
+    val hasil = luasSegitiga(6, 12)
+
+    println(hasil)
+}
+
+fun luasSegitiga(alas: Int, tinggi: Int): Int{
+    return alas * tinggi / 2
+}
