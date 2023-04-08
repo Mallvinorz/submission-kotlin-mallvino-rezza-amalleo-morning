262db70cbe3fc422461b40f85d1a826798fc111a
mallvinorz <mallvino1216@gmail.com>
2023-04-08 12:37:58 +0700

    add CollectionFunction-Count.kt

---
+++ src/main/kotlin/KotlinCollection/CollectionFunction-Count.kt
@@ -0,0 +1,9 @@
+package KotlinCollection
+
+fun main() {
+    val foods = arrayOf("Pizza", "Burger", "Fettuccine", "Basreng", "Batagor")
+
+    val countFoods = foods.count()
+
+    println(countFoods)
+}
