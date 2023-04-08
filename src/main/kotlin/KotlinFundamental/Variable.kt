3705fa68aef823ce48842a2a3530cfef9331f139
mallvinorz <mallvino1216@gmail.com>
2023-04-08 11:51:53 +0700

    add Variable.kt

---
+++ src/main/kotlin/KotlinFundamental/Variable.kt
@@ -0,0 +1,11 @@
+package KotlinFundamental
+
+fun main() {
+    var name: String = "Rezza"
+    name = "Mallvino Rezza Amalleo"
+    println(name)
+
+//    val name: String = "Rezza"
+//    name = "Mallvino Rezza Amalleo" // val cannot be reassigned
+//    println(name)
+}
