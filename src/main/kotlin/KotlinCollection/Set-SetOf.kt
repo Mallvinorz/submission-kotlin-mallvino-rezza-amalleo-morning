ca673c8968c9232e951dcb2f72b0fb4c040685d6
mallvinorz <mallvino1216@gmail.com>
2023-04-08 12:24:49 +0700

    add Set-SetOf.kt

---
+++ src/main/kotlin/KotlinCollection/Set-SetOf.kt
@@ -0,0 +1,11 @@
+package KotlinCollection
+
+fun main() {
+    val bbKelasA=  setOf(56, 78, 62, 55, 55, 45, 60, 53, 78, 60)
+    val bbKelasB = setOf(45, 53, 55, 56, 60, 62, 78)
+
+    val hasil = bbKelasA == bbKelasB
+    println(hasil)
+
+
+}
