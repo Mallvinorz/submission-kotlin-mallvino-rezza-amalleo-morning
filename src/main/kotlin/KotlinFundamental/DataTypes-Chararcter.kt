52a36487970f3088c2ab0bcc54147f9476f95f01
mallvinorz <mallvino1216@gmail.com>
2023-04-08 11:55:43 +0700

    add DataTypes-Chararcter.kt

---
+++ src/main/kotlin/KotlinFundamental/DataTypes-Chararcter.kt
@@ -0,0 +1,15 @@
+package KotlinFundamental
+
+fun main() {
+    var char = 'Y'
+
+    println("Character " + char--)
+    println("Character " + char--)
+    println("Character " + char--)
+    println("")
+    println("Character " + char++)
+    println("")
+    println("Character " + char++)
+    println("Character " + char++)
+    println("Character " + char++)
+}
