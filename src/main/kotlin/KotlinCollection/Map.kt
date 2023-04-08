ed57095409cf48f6e5165f92bba43bf37f03c693
mallvinorz <mallvino1216@gmail.com>
2023-04-08 12:30:55 +0700

    add Map.kt

---
+++ src/main/kotlin/KotlinCollection/Map.kt
@@ -0,0 +1,20 @@
+package KotlinCollection
+
+fun main() {
+    val groupMap = mapOf(
+        1 to "Fender Stratocaster",
+        2 to "Fender Telecaster",
+        3 to "Squier Stratocaster",
+        4 to "Squier Telecaster",
+        5 to "Fender Jaguar",
+        6 to "Squier Jaguar",
+        7 to "Takamine",
+        8 to "Taylor",
+    )
+
+    println(groupMap)
+    println(groupMap[7])
+    println(groupMap.getValue(4))
+    println(groupMap.values)
+    println(groupMap.keys)
+}
