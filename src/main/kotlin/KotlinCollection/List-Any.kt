7ca013997c9847bbf1cc2369489679bfc9826df3
mallvinorz <mallvino1216@gmail.com>
2023-04-08 12:22:38 +0700

    add List-Any.kt

---
+++ src/main/kotlin/KotlinCollection/List-Any.kt
@@ -0,0 +1,13 @@
+package KotlinCollection
+
+fun main() {
+    val temperatureList = mutableListOf("Celcius", 100, true, 27.9)
+    println(temperatureList)
+
+    temperatureList.remove("Celsius")
+    temperatureList.set(0, 83)
+    temperatureList[1] = 35
+    temperatureList.add(3, 56.3)
+
+    println(temperatureList)
+}
