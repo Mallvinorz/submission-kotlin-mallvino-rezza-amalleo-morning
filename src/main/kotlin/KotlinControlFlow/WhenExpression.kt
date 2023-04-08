a800b405e552cffcc2a82ec604f0358c7bba11bb
mallvinorz <mallvino1216@gmail.com>
2023-04-08 13:46:46 +0700

    add WhenExpression.kt

---
+++ src/main/kotlin/KotlinControlFlow/WhenExpression.kt
@@ -0,0 +1,16 @@
+package KotlinControlFlow
+
+fun main() {
+    val menuTerpilih = 4
+
+    val menu = when (menuTerpilih) {
+        1 -> "Sirloin Steak"
+        2 -> "Tenderloin Steak"
+        3 -> "Chicken Steak"
+        4 -> "Dori Steak"
+
+        else -> "Invalid Number"
+    }
+
+    println(menu)
+}
