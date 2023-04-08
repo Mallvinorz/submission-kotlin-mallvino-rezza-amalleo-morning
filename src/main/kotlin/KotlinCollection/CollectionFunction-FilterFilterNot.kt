a4df2fa6abdd0290846fede3e03477aa87d1bb0d
mallvinorz <mallvino1216@gmail.com>
2023-04-08 12:35:28 +0700

    add CollectionFunction-FilterFilterNot.kt

---
+++ src/main/kotlin/KotlinCollection/CollectionFunction-FilterFilterNot.kt
@@ -0,0 +1,12 @@
+package KotlinCollection
+
+fun main() {
+    val listBilangan = listOf(3, 4, 6, 8, 12, 16, 24, 32)
+
+    val kelipatanTiga = listBilangan.filter { it % 3 == 0 }
+
+    val kelipatanEmpat = listBilangan.filterNot { it % 3 == 0 }
+
+    println("Berikut bilangan kelipatan 3, diantaranya $kelipatanTiga")
+    println("Sedangkan untuk bilangan kelipatan 4, diantaranya $kelipatanEmpat")
+}
