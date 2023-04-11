26ab656e2f8c603bc8954caa3c7c306b74e2664b
mallvinorz <mallvino1216@gmail.com>
2023-04-11 16:40:53 +0700

    add StringTemplate.kt

---
+++ src/main/kotlin/kotlinFundamental/StringTemplate.kt
@@ -0,0 +1,13 @@
+package kotlinFundamental
+
+fun main() {
+//    Without String Template
+    val nama = "Mallvino Rezza Amalleo"
+    println("Halo perkenalkan nama saya " + nama)
+
+//    Using String Template
+    val tempatWisata = "Jatim Park 2"
+    print("Halo guys kembali lagi sama gue $nama, kali ini gue sedang di $tempatWisata")
+
+
+}
