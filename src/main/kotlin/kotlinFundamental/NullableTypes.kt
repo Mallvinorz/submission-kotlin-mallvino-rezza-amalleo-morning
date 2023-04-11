5e287b13ede3fac02da7e62795c4bfdb9cdc864b
mallvinorz <mallvino1216@gmail.com>
2023-04-11 16:22:08 +0700

    modify NullableTypes.kt

--- src/main/kotlin/kotlinFundamental/NullableTypes.kt
+++ src/main/kotlin/kotlinFundamental/NullableTypes.kt
@@ -2,5 +2,14 @@ package kotlinFundamental
 
 fun main() {
     //Safe calls & Elvis Operator
-    var total: Int? = null
+    var dateIn: String? = null
+    var statusKamar = "Booked"
+
+    statusKamar = "Check In"
+
+    if (statusKamar.equals("Check In")) {
+        dateIn = "11/4/2023 16:16:10 WIB"
+
+        println(dateIn)
+    }
 }
